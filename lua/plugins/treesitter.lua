a58592f1d67b6daad5fbab2e33d2d24480522faa
SamirArioui <arioui.samir@gmail.com>
2024-01-14 20:09:05 +0100

    refactor: update treesitter optons

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,9 +1,5 @@
 local opts = {
     ensure_installed = { "rust", "c", "lua", "vim", "vimdoc", "query", "python", "bash", "typescript", "javascript", "html", "css" },
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-    },
     indent = { enable = true },
 }
 
