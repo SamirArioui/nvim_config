81c6269760999251b908b73372313e171d4b77fc
SamirArioui <arioui.samir@gmail.com>
2024-01-14 19:42:47 +0100

    refactor: rework lazygit.lua file

--- init.lua
+++ init.lua
@@ -2,8 +2,12 @@
 require("core.launch")
 require("core.options")
 require("core.keymaps")
+
+-- plugins
 spec("plugins.colorscheme")
 spec("plugins.devicon")
 spec("plugins.treesitter")
+spec("plugins.lazygit")
+
 -- load lazy (put in last of init.lua)
 require("plugins.lazy")
--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,9 +1,9 @@
-return {
-    {
-        "kdheepak/lazygit.nvim",
-        -- optional for floating window border decoration
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-        },
+local M = {
+    "kdheepak/lazygit.nvim",
+    -- optional for floating window border decoration
+    dependencies = {
+        "nvim-lua/plenary.nvim",
     },
 }
+
+return M
