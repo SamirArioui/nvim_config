b3b7f9a8d7779977089df43740008d172cacd927
SamirArioui <arioui.samir@gmail.com>
2024-01-15 10:00:31 +0100

    refactor: change colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,12 @@
 local M = {
-    "lunarvim/darkplus.nvim",
-    lazy = false,
+	"catppuccin/nvim",
+	name = "catppuccin",
+	lazy = false,
 	priority = 1000,
 }
 
 function M.config()
-    vim.cmd("colorscheme darkplus")
+	vim.cmd("colorscheme catppuccin-mocha")
 end
 
 return M
