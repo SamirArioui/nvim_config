68b52cde5d9dc1253e96f898911fb73d14e28052
SamirArioui <arioui.samir@gmail.com>
2024-01-20 20:12:14 +0100

    refactor: update lualine spec

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,46 @@
+local opts = function()
+  local Util = require("lazyvim.util")
+  local icons = require("lazyvim.config").icons
+  return {
+    options = {
+      theme = "auto",
+      globalstatus = true,
+      disabled_filetypes = { statusline = { "dashboard", "alpha", "starter" } },
+    },
+    sections = {
+      lualine_a = { "mode" },
+      lualine_b = { "branch" },
+
+      lualine_c = {
+        Util.lualine.root_dir(),
+        {
+          "diagnostics",
+          symbols = {
+            error = icons.diagnostics.Error,
+            warn = icons.diagnostics.Warn,
+            info = icons.diagnostics.Info,
+            hint = icons.diagnostics.Hint,
+          },
+        },
+      },
+      lualine_z = { "filetype" },
+    },
+    winbar = {
+      lualine_a = {
+        {
+          "filename",
+          symbols = {
+            modified = icons.git.modified,
+          },
+          file_status = true,
+          path = 4,
+        },
+      },
+    },
+  }
+end
+
+return {
+  "nvim-lualine/lualine.nvim",
+  opts = opts(),
+}
