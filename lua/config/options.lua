fed621d9f49d279cd388ff4f43f0c2944783c7ee
SamirArioui <arioui.samir@gmail.com>
2024-01-21 00:05:40 +0100

    refactor: update options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,10 +3,12 @@ vim.g.maplocalleader = "\\"
 
 local opt = vim.opt
 
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.softtabstop = 4
 opt.confirm = false -- Confirm to save changes before exiting modified buffer
 opt.scrolloff = 8 -- Lines of context
 opt.shiftround = true -- Round indent
-opt.shiftwidth = 4 -- Size of an indent
 opt.sidescrolloff = 8 -- Columns of context
 opt.colorcolumn = "80"
 opt.swapfile = false
