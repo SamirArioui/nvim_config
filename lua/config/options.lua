65a94e7b2013272b54c2e0f7f930e9757979e752
sarioui <arioui.samir@gmail.com>
2024-07-08 10:34:37 +0200

    feat: update options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,17 +1,3 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
-local opt = vim.opt
-
-opt.tabstop = 4
-opt.shiftwidth = 4
-opt.softtabstop = 4
-opt.confirm = false -- Confirm to save changes before exiting modified buffer
-opt.scrolloff = 8 -- Lines of context
-opt.shiftround = true -- Round indent
-opt.sidescrolloff = 8 -- Columns of context
-opt.colorcolumn = "80"
-opt.swapfile = false
-opt.backup = false
-opt.iskeyword:append("-")
-opt.clipboard = "unnamedplus"
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
